60db7d59f0b70fc29d256d6676183e967be9c1f5
Luckystrike561 <luc56220@gmail.com>
2026-01-19 14:12:03 +0100

    chore: bump homebrew formula to v0.1.5

--- homebrew/jarvis.rb
+++ homebrew/jarvis.rb
@@ -1,8 +1,8 @@
 class Jarvis < Formula
   desc "Beautiful TUI for managing and executing scripts with zero configuration"
   homepage "https://github.com/Luckystrike561/jarvis"
-  url "https://github.com/Luckystrike561/jarvis/archive/refs/tags/v0.1.4.tar.gz"
-  sha256 "f9fa751103762efde68f1eb3e584ae8d87fa3a97b669968791221d6bd78f9dd4"
+  url "https://github.com/Luckystrike561/jarvis/archive/refs/tags/v0.1.5.tar.gz"
+  sha256 "39595e22fcc29120dc8ca2be57f12cced32cefe927ff52a204d4dea89daa92e1"
   license "MIT"
   head "https://github.com/Luckystrike561/jarvis.git", branch: "main"
 
